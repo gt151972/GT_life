21d187fb9eb31d8d09bae069bdae809eb25937b7
GT mac <gtmac@GT.lan>
2015-09-25 11:03:29 +0800

    创建GT_life,用swift编写

---
+++ GT_life/GT_lifeTests/GT_lifeTests.swift
@@ -0,0 +1,36 @@
+//
+//  GT_lifeTests.swift
+//  GT_lifeTests
+//
+//  Created by GT mac on 15/9/25.
+//  Copyright © 2015年 GT mac. All rights reserved.
+//
+
+import XCTest
+@testable import GT_life
+
+class GT_lifeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
